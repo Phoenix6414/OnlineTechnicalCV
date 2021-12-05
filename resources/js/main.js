1e67b491ff763dd4ad450fe7a66d1688df852491
Phoenix6414 <56797107+Phoenix6414@users.noreply.github.com>
2021-12-05 13:57:05 +0200

    Image Change

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,22 +3,22 @@ const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
     
-    // Validate that variables exist
+    
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
-            // We add the show-menu class to the div tag with the nav__menu class
+            
             nav.classList.toggle('show-menu')
         })
     }
 }
 showMenu('navToggle','navMenu')
 
-// remove menue - small screen 
+// remove menue - big screen 
 const navLink = document.querySelectorAll('.nav-link')
 
 function linkAction(){
     const navMenu = document.getElementById('navMenu')
-    // When we click on each nav__link, we remove the show-menu class
+    
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
@@ -44,15 +44,16 @@ function scrollActive(){
 }
 window.addEventListener('scrollTop', scrollActive)
 
+//change themes
 const themeButton = document.getElementById('theme-btn')
 const darkTheme = 'dark-theme'
 const iconTheme = 'bxs-sun'
 
-// Previously selected topic (if user selected)
+
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-// We obtain the current theme that the interface has by validating the dark-theme class
+
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bxs-moon' : 'bxs-sun'
 
